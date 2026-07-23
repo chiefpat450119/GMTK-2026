d907ab388c59565119a4396b3cb56c2cf9772e95
Yikachu <leon.liang.2004@gmail.com>
2026-07-23 15:38:20 -0700

    make projectile bounce and finish map test scene

--- scenes/entities/projectiles/projectile.gd
+++ scenes/entities/projectiles/projectile.gd
@@ -6,4 +6,6 @@ func _enter_tree() -> void:
 	velocity = Vector2(speed, 0).rotated(rotation)
 
 func _physics_process(delta: float) -> void:
-	move_and_slide()
+	var collision := move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
