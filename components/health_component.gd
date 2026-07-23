d7993ac6058040acd6a87b4c3abb63f04b2b943c
aido <pieandseal@gmail.com>
2026-07-23 01:45:28 -0700

    feat: player hp changed event
    requested by leon for ui things

--- components/health_component.gd
+++ components/health_component.gd
@@ -5,6 +5,8 @@ extends Node
 
 @onready var hp := max_hp.current_val()
 
+@export var hp_changed_event : GameEvent
+
 func add_hp(amount: float) -> void:
 	_set_hp(hp + amount)
 
@@ -15,3 +17,4 @@ func remove_hp(amount: float) -> void:
 
 func _set_hp(value: float) -> void:
 	hp = clamp(value, 0, max_hp)
+	hp_changed_event.raise()
