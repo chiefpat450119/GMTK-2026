5aa50d21485a454676ca2db370f02aeb4d2fba66
CadenH <whyamisonotawesome@gmail.com>
2026-07-23 15:08:51 -0600

    Added collectable sand
    Added a collectable sand entity with a script that has an optional despawn timer and adds sand+self deletes upon collection, right now there is a singleton that spawns it which can certainly be changed later, also added a placeholder png

---
+++ scenes/entities/collectables/collectable_sand.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var pickup_amount: int = 2
+@export var despawn_time: float = -1.0
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	#A despawn timer, set despawn time to a number greater than zero for use
+	if despawn_time > 0:
+		await get_tree().create_timer(10).timeout
+		queue_free()
+
+	
+func _on_body_entered(body):
+	#as sand is being picked up it adds time I think, then it deletes the item
+	PlayerManager.add_time(pickup_amount)
+	queue_free()
