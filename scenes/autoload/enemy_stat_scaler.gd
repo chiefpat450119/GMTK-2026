8361910be03d35cdd08df6b653924e0b3980d7af
winterSteve25 <w1nter.ish3re@gmail.com>
2026-07-24 01:09:26 -0700

    Take out const

--- scenes/autoload/enemy_stat_scaler.gd
+++ scenes/autoload/enemy_stat_scaler.gd
@@ -2,6 +2,10 @@ class_name EnemyStatScaler
 extends Node
 
 const SCALING_MOD_ID := &"time_scaling"
+const HP_MUL = 0.03
+const ATK_MUL = 0.02
+const SPD_MUL = 0.006
+const RANGE_MUL = 0.004
 
 @export var hp_stat: Stat
 @export var atk_stat: Stat
@@ -17,10 +21,10 @@ func _ready() -> void:
 
 func tick() -> void:
     _total += 1
-    _scale(hp_stat, 0.030 * _total)     # +3.0% max hp
-    _scale(atk_stat, 0.020 * _total)    # +2.0% attack
-    _scale(speed_stat, 0.006 * _total)  # +0.6% move speed
-    _scale(range_stat, 0.004 * _total)  # +0.4% range
+    _scale(hp_stat, HP_MUL * _total)     # +3.0% max hp
+    _scale(atk_stat, ATK_MUL * _total)    # +2.0% attack
+    _scale(speed_stat, SPD_MUL * _total)  # +0.6% move speed
+    _scale(range_stat, RANGE_MUL * _total)  # +0.4% range
 
 func reset() -> void:
     _total = 0
