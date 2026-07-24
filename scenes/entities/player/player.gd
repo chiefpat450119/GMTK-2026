2d3d40020338ab56d31822b7edf5c7c3bc9b0f7c
GamesFoundation <nicholas.liang.2006@gmail.com>
2026-07-23 23:49:42 -0700

    feat: added player character idle sprites
    Added the idle sprites for the player to an animated sprite node where it just loops the idle animation for now
    
    Player also faces towards the direction he's moving in

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -14,6 +14,7 @@ static var instance: Player
 @export var time_component : TimeComponent
 @export var movement_component : MovementComponent
 @export var gun : Gun
+@export var sprite : AnimatedSprite2D
 
 
 func _enter_tree() -> void:
@@ -26,6 +27,11 @@ func _enter_tree() -> void:
 func _physics_process(_delta: float) -> void:
 	var dir = Input.get_vector("Left", "Right", "Up", "Down")
 	movement_component.move(dir)
+	
+	if Input.get_vector("Left", "Right", "Up", "Down") == Vector2.LEFT:
+		sprite.flip_h = true
+	elif Input.get_vector("Left", "Right", "Up", "Down") == Vector2.RIGHT:
+		sprite.flip_h = false
 	
 	if Input.is_action_pressed("M1"):
 		gun.shoot()
