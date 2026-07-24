76b1e15504c4f876e2ff68d9050377bd5c6bedb5
Caden Zhang <w1nter.ish3re@gmail.com>
2026-07-23 20:53:23 -0700

    Get rid of base attack mul

--- components/atk_component.gd
+++ components/atk_component.gd
@@ -1,8 +1,7 @@
 class_name AtkComponent
 extends Node
 
-@onready var base_atk_mul: float = 1
 @export var atk_stat: Stat
 
 func attack(damage: float, target: HealthComponent):
-	target.remove_hp(atk_stat.current_val(damage * base_atk_mul))
+	target.remove_hp(atk_stat.current_val(damage))
