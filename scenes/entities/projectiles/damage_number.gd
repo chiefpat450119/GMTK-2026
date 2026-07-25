69da47e7f7d39d9ae2f4542424375e2c2d431338
winterSteve25 <w1nter.ish3re@gmail.com>
2026-07-25 11:23:12 -0700

    fix styling and remove rng creation

--- scenes/entities/projectiles/damage_number.gd
+++ scenes/entities/projectiles/damage_number.gd
@@ -9,25 +9,24 @@ func _ready() -> void:
 	
 	# A little bit of random offset so popups don't overlap 
 	# when there are many of them at once
-	var rng = RandomNumberGenerator.new()
-	label.position.x += rng.randf_range(-10,10)
-	label.position.y += rng.randf_range(-10,10)
+	label.position.x += randf_range(-10,10)
+	label.position.y += randf_range(-10,10)
 	
 	# Little bit of random colour variation
-	label.modulate.g += rng.randf_range(0, 0.6)
+	label.modulate.g += randf_range(0, 0.6)
 	
 	# Float upwards
 	var tween1 := create_tween()
-	var dest := label.position + Vector2(rng.randf_range(-20,20), -20)
+	var dest := label.position + Vector2(randf_range(-20,20), -20)
 	var duration := 1
 	tween1.tween_property(label, "position", dest, duration)\
-	.set_ease(Tween.EASE_OUT)\
-	.set_trans(Tween.TRANS_EXPO)
+            .set_ease(Tween.EASE_OUT)\
+            .set_trans(Tween.TRANS_EXPO)
 	
 	# Turn transparent
 	var tween2 := create_tween()
 	duration = 1
 	tween2.tween_property(self, "modulate", Color.TRANSPARENT, duration)\
-	.set_ease(Tween.EASE_IN)\
-	.set_trans(Tween.TRANS_CUBIC)
+            .set_ease(Tween.EASE_IN)\
+            .set_trans(Tween.TRANS_CUBIC)
 	tween2.tween_callback(queue_free)
