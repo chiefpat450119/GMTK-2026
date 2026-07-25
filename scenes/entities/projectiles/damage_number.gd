66d5f56f1bef454b8b14b2b2508373b066a2ca03
winterSteve25 <w1nter.ish3re@gmail.com>
2026-07-25 11:26:42 -0700

    Delete temp files clean up damange numbers file a bit

--- scenes/entities/projectiles/damage_number.gd
+++ scenes/entities/projectiles/damage_number.gd
@@ -9,24 +9,22 @@ func _ready() -> void:
 	
 	# A little bit of random offset so popups don't overlap 
 	# when there are many of them at once
-	label.position.x += randf_range(-10,10)
-	label.position.y += randf_range(-10,10)
+	label.position.x += randf_range(-40, 40)
+	label.position.y += randf_range(-40, 40)
 	
 	# Little bit of random colour variation
 	label.modulate.g += randf_range(0, 0.6)
 	
 	# Float upwards
-	var tween1 := create_tween()
-	var dest := label.position + Vector2(randf_range(-20,20), -20)
-	var duration := 1
-	tween1.tween_property(label, "position", dest, duration)\
+	var tween := create_tween()
+	var dest := Vector2(randf_range(-20, 20), -40)
+	const duration := 1
+	tween.parallel().tween_property(label, "position", dest, duration)\
+            .as_relative()\
             .set_ease(Tween.EASE_OUT)\
             .set_trans(Tween.TRANS_EXPO)
-	
-	# Turn transparent
-	var tween2 := create_tween()
-	duration = 1
-	tween2.tween_property(self, "modulate", Color.TRANSPARENT, duration)\
+	tween.parallel().tween_property(self, "modulate", Color.TRANSPARENT, duration)\
             .set_ease(Tween.EASE_IN)\
             .set_trans(Tween.TRANS_CUBIC)
-	tween2.tween_callback(queue_free)
+
+	tween.tween_callback(queue_free)
