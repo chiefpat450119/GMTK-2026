728a011f59aba1109568eda7ae6f9cc05b41fbef
aido <pieandseal@gmail.com>
2026-07-25 21:02:58 -0700

    boss anim implementation
    im tired

--- scenes/entities/enemies/boss/boss_attack_state.gd
+++ scenes/entities/enemies/boss/boss_attack_state.gd
@@ -5,21 +5,22 @@ extends State
 
 @export var atk_component: AtkComponent
 
-@export var state_time: float = 2
+#@export var state_time: float = 2
 
-@export var sprite: Sprite2D
+#@export var sprite: Sprite2D
 
 
 func enter():
 	super()
 	
+	var yield_time := play_anim(&"Swing")
 	# TEMP
-	var tween := create_tween()
-	tween.tween_property(sprite, "rotation_degrees", 360, 0.1) \
-		.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
-	
+	#var tween := create_tween()
+	#tween.tween_property(sprite, "rotation_degrees", 360, 0.1) \
+		#.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	#
 	atk_component.set_active(true)
-	await get_tree().create_timer(state_time).timeout
+	await get_tree().create_timer(yield_time).timeout
 	atk_component.set_active(false)
 	
 	switch_state(selector_state)
--- scenes/entities/enemies/boss/boss_clock_atk_state.gd
+++ scenes/entities/enemies/boss/boss_clock_atk_state.gd
@@ -16,12 +16,27 @@ func enter():
 	
 	switch_state(selector_state)
 
+func delay(time: float):
+	await get_tree().create_timer(time).timeout
+
+
 func windup():
+	
 	clock_instance = clock_attack.instantiate()
-	enemy.add_child(clock_instance)
+	enemy.add_child.call_deferred(clock_instance)
+	
+	clock_instance.expand_clock_edge()
+	
+	await delay(0.1)
+	
+	clock_instance.expand_hand()
+	
+	await delay(0.5)
 	
 	clock_instance.orient_numbers()
-	await get_tree().create_timer(windup_time).timeout
+	
+
+	await get_tree().create_timer(play_anim(&"Clock")).timeout
 
 func swing():
 	await clock_instance.swing_hand()
