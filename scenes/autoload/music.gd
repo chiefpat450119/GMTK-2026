c5d915ef3682bc35699155d3fefff8e0a806cd0c
chiefpat450119 <patrickzhou45@gmail.com>
2026-08-02 00:05:47 -0400

    refactor music

--- scenes/autoload/music.gd
+++ scenes/autoload/music.gd
@@ -3,7 +3,6 @@ extends Node
 @export var duck_db: float = -12.0
 @export var fade_duration: float = 0.35
 
-@export var wave_end_event: GameEventListener
 @export var boss_event: GameEventListener
 
 @export var battle_player: AudioStreamPlayer
@@ -12,10 +11,8 @@ extends Node
 
 var _active_player: AudioStreamPlayer
 var _full_db: Dictionary[AudioStreamPlayer, float] = {}
-var _saved_position: Dictionary[AudioStreamPlayer, float] = {}
 var _fade: Tween
-var _audio_players: Array[AudioStreamPlayer] = []
-var _boss_spawned: bool = false
+var _boss_active: bool = false
 
 
 func _ready() -> void:
@@ -25,47 +22,21 @@ func _ready() -> void:
 	_full_db[boss_player] = boss_player.volume_db
 	_full_db[menu_player] = menu_player.volume_db
 
-	for node: Node in get_children():
-		if node is AudioStreamPlayer:
-			_audio_players.append(node)
-
 	_active_player = battle_player
 
 	GameStateManager.instance.state_changed.connect(_on_state_changed)
-
-	wave_end_event.response.connect(_on_wave_end)
 	boss_event.response.connect(_on_boss_start)
 
 	_apply(GameStateManager.instance.state)
 
 
-func _on_state_changed(from: int, to: int) -> void:
-	# Returning from the upgrade screen after a normal wave starts the battle
-	# music again. A boss event can select boss_player before this happens.
-	if (
-		to == GameStateManager.GameState.PLAYING
-		and from == GameStateManager.GameState.UPGRADING
-		and _active_player == null
-	):
-		_active_player = battle_player
-
+func _on_state_changed(_from: int, to: int) -> void:
 	_apply(to)
 
 
-func _on_wave_end() -> void:
-	_cancel_fade()
-
-	_saved_position[battle_player] = battle_player.get_playback_position()
-	battle_player.stop()
-	battle_player.stream_paused = false
-	battle_player.volume_db = _full_db[battle_player]
-
-	if _active_player == battle_player:
-		_active_player = null
-
 
 func _on_boss_start() -> void:
-	_boss_spawned = true
+	_boss_active = true
 	_apply(GameStateManager.instance.state)
 
 
@@ -74,10 +45,8 @@ func _switch_track(next_player: AudioStreamPlayer) -> void:
 		return
 	_cancel_fade()
 
-	if _active_player != null and _active_player != next_player:
-		_saved_position[_active_player] = _active_player.get_playback_position()
-		_active_player.stop()
-		_active_player.stream_paused = false
+	if _active_player != null:
+		_active_player.stream_paused = true
 		_active_player.volume_db = _full_db[_active_player]
 
 	_active_player = next_player
@@ -85,7 +54,7 @@ func _switch_track(next_player: AudioStreamPlayer) -> void:
 func _select_track(state: int) -> void:
 	match state:
 		GameStateManager.GameState.PLAYING, GameStateManager.GameState.UPGRADING:
-			if _boss_spawned:
+			if _boss_active:
 				_switch_track(boss_player)
 			else:
 				_switch_track(battle_player)
@@ -117,7 +86,7 @@ func _apply(state: int) -> void:
 
 			# Every new run begins with the battle track selected.
 			_switch_track(battle_player)
-			_boss_spawned = false
+			_boss_active = false
 
 
 func _play_active(ducked: bool) -> void:
@@ -125,23 +94,17 @@ func _play_active(ducked: bool) -> void:
 		return
 
 	var target_db: float = _full_db[_active_player]
-
 	if ducked:
 		target_db += duck_db
 
-	var running := (
-		_active_player.playing
-		or _active_player.stream_paused
-	)
+	var running := _active_player.playing or _active_player.stream_paused
 
 	_active_player.stream_paused = false
 
 	if not running:
 		_cancel_fade()
 		_active_player.volume_db = target_db
-		var resume_pos: float = _saved_position.get(_active_player, 0.0)
-		_saved_position.erase(_active_player)
-		_active_player.play(resume_pos)
+		_active_player.play()
 		return
 
 	_fade_to(target_db)
@@ -155,9 +118,8 @@ func _pause_active() -> void:
 
 func _stop_all() -> void:
 	_cancel_fade()
-	_saved_position.clear()
 
-	for music_player: AudioStreamPlayer in _audio_players:
+	for music_player: AudioStreamPlayer in [battle_player, boss_player, menu_player]:
 		music_player.stop()
 		music_player.stream_paused = false
 		music_player.volume_db = _full_db[music_player]
